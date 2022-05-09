3ac9d5b7f5371106ae64437fe391b199f8985ca9
Jake Teton-Landis <just.1.jake@gmail.com>
2022-05-08 21:24:40 -0700

    get ye wasm-pack

--- vercel-build.sh
+++ vercel-build.sh
@@ -6,5 +6,15 @@ if rustup -V ; then
   echo "Rust already installed"
 else
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain stable
+  source "$HOME/.cargo/env"
 fi
+
+if wasm-pack -V ; then
+  echo "WASM-Pack already installed"
+else
+  curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+fi
+
+cargo install
+
 ./build-percival-wasm-with-typescript-types.sh
